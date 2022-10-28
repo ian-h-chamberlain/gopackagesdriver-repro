d2dccab2637cb4ecb3ce3fa9316c3ffcaaa43e5e
Ian Chamberlain <ichamberlain@juniper.net>
2022-10-28 14:44:56 -0400

    Add gopackagesdriver with NotHandled:true

--- gopackagesdriver.sh
+++ gopackagesdriver.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-exec bazel run -- @io_bazel_rules_go//go/tools/gopackagesdriver "$@"
+if [[ $1 = *xyz* ]]; then
+    sleep 3
+    echo '{"NotHandled":true,"Sizes":{"WordSize":8,"MaxAlign":8},"Packages":[]}'
+else
+    exec bazel run -- @io_bazel_rules_go//go/tools/gopackagesdriver "$@" | tee >> packagesdriver.log
+fi
